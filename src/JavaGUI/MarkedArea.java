b2a227bd808be1417f280f6a0836427fdc16fa21
janna <janna>
2003-11-11 13:18:18 +0200

    *** empty log message ***

---
+++ src/JavaGUI/MarkedArea.java
@@ -0,0 +1,15 @@
+//package javaGUI;                                 
+  public class MarkedArea 
+  {
+    public int begin;
+    public int end;
+    public String position;
+
+    MarkedArea(int b, int e, String p)
+    {
+      begin = b;
+      end = e;
+      position = p;
+    }
+  }
+
