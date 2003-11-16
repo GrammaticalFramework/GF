9d55f72d7a97658faa6ebc890535fa0c6e665a05
janna <unknown>
2003-11-16 11:25:52 +0000

    *** empty log message ***

--- src/JavaGUI/MarkedArea.java
+++ src/JavaGUI/MarkedArea.java
@@ -4,12 +4,14 @@
     public int begin;
     public int end;
     public String position;
+    public String words;
 
-    MarkedArea(int b, int e, String p)
+    MarkedArea(int b, int e, String p, String w)
     {
       begin = b;
       end = e;
       position = p;
+      words = w;
     }
   }
 
