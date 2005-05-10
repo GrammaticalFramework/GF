6b28408fe1ead384b13934c72aca8674dd863735
bringert <unknown>
2005-05-10 14:04:01 +0000

    Made conditional compilation of readline stuff cleaner

--- src/GF/System/Readline.hs
+++ src/GF/System/Readline.hs
@@ -7,9 +7,9 @@
 -- Stability   : (stable)
 -- Portability : (portable)
 --
--- > CVS $Date: 2005/05/10 14:55:01 $ 
+-- > CVS $Date: 2005/05/10 15:04:01 $ 
 -- > CVS $Author: bringert $
--- > CVS $Revision: 1.1 $
+-- > CVS $Revision: 1.2 $
 --
 -- Uses the right readline library to read user input.
 -----------------------------------------------------------------------------
@@ -18,34 +18,10 @@ module GF.System.Readline (fetchCommand) where
 
 #ifdef USE_READLINE
 
-import System.Console.Readline (readline, addHistory)
+import GF.System.UseReadline (fetchCommand)
 
 #else
 
-import System.IO.Error (try)
-import System.IO (stdout,hFlush)
-
-#endif
-
-#ifdef USE_READLINE
-
-fetchCommand :: String -> IO (String)
-fetchCommand s = do
-  res <- readline s
-  case res of
-   Nothing -> return "q"
-   Just s -> do addHistory s
-                return s
-
-#else
-
-fetchCommand :: String -> IO (String)
-fetchCommand s = do
-  putStr s
-  hFlush stdout
-  res <- try getLine
-  case res of
-   Left e -> return "q"
-   Right l -> return l
+import GF.System.NoReadline (fetchCommand)
 
 #endif
