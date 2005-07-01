76d50368fb1a1fbdae286191809950a68129a87e
hdaniels <unknown>
2005-07-01 08:47:21 +0000

    Eliminated result as a global variable

--- src/JavaGUI2/de/uka/ilkd/key/ocl/gf/LinPosition.java
+++ src/JavaGUI2/de/uka/ilkd/key/ocl/gf/LinPosition.java
@@ -21,7 +21,7 @@ package de.uka.ilkd.key.ocl.gf;
  * if all hold (correct/incorrect). 
  * Class is immutable.
  */
-public class LinPosition {
+class LinPosition {
         /**
          * a position in the AST in Haskell notation
          */
@@ -40,6 +40,64 @@ public class LinPosition {
         LinPosition(String p, boolean cor) {
                 position = p;
                 correctPosition = cor;
+        }
+        
+        /**
+         * Creates a position string in Haskell notation for the argument 
+         * number nr of this node.
+         * @param nr The number of the wanted argument
+         * @return the position string for the nrth child
+         */
+        public String childPosition(int nr) {
+                return this.position.substring(0, this.position.length() - 1) + "," + nr + "]";
+        }
+
+        /**
+         * compares two position strings and returns true, if superPosition is
+         * a prefix of subPosition, that is, if subPosition is in a subtree of
+         * superPosition
+         * @param superPosition the position String in Haskell notation 
+         * ([0,1,0,4]) of the to-be super-branch of subPosition
+         * @param subPosition the position String in Haskell notation 
+         * ([0,1,0,4]) of the to-be (grand-)child-branch of superPosition
+         * @return true iff superPosition denotes an ancestor of subPosition 
+         */
+        public static boolean isSubtreePosition(final LinPosition superPosition, final LinPosition subPosition) {
+                if (superPosition == null || subPosition == null) {
+                        return false;
+                }
+                String superPos = superPosition.position;
+                String subPos = subPosition.position;
+                if (superPos.length() < 2 || subPos.length() < 2 ) {
+                        return false;
+                }
+                superPos = superPos.substring(1, superPos.length() - 1);
+                subPos = subPos.substring(1, subPos.length() - 1);
+                boolean result = subPos.startsWith(superPos);
+                return result;
+        }     
+        
+        /** 
+         * Returns the biggest position of first and second.
+         * Each word in the linearization area has the corresponding
+         * position in the tree. The position-notion is taken from 
+         * GF-Haskell, where empty position ("[]") 
+         * represents tree-root, "[0]" represents  first child of the root,
+         * "[0,0]" represents the first grandchild of the root etc.
+         * So comparePositions("[0]","[0,0]")="[0]"
+         */
+        public static String maxPosition(String first, String second) {
+                String common ="[]";
+                int i = 1; 
+                while ((i<Math.min(first.length()-1,second.length()-1))&&(first.substring(0,i+1).equals(second.substring(0,i+1)))) {
+                        common=first.substring(0,i+1); 
+                        i+=2;
+                }
+                if (common.charAt(common.length()-1)==']') {
+                        return common;
+                } else { 
+                        return common+"]";
+                }
         }
         
         public String toString() {
