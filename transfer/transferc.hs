7d66bb8ef6214e33d76468e2bde59deb72767796
bringert <bringert@cs.chalmers.se>
2006-09-13 13:46:04 +0200

    transferc now uses env var TRANSFER_PATH to locate imports.

--- transfer/transferc.hs
+++ transfer/transferc.hs
@@ -12,14 +12,28 @@ die s = do
         hPutStrLn stderr s
         exitFailure
 
+getPath :: IO [String]
+getPath = do env <- getEnvironment
+             return $ case lookup "TRANSFER_PATH" env of
+                        Nothing -> []
+                        Just p  -> splitBy (==':') p
+
+-- Modified version of a function which is originally
+-- Copyright Bryn Keller
+splitBy :: (a -> Bool) -> [a] -> [[a]]
+splitBy _ [] = []
+splitBy f list = first : splitBy f (dropWhile f rest)
+   where (first, rest) = break f list
+
 main :: IO ()
 main = do 
        args <- getArgs
        let (flags,files) = partition ("-" `isPrefixOf`) args
-           path = [ p | ('-':'i':p) <- flags ]
+           argPath = [ p | ('-':'i':p) <- flags ]
+       envPath <- getPath
        case files of
          [f] -> do
-                cf <- compileFile path f
+                cf <- compileFile (argPath ++ envPath) f
                 putStrLn $ "Wrote " ++ cf
                 return ()
          _   -> die "Usage: transferc [-i<path> [-i<path> ... ]] <file>"
