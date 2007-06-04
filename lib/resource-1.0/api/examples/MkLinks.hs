f7aaed3822128ccd2d55c6490056f45fa93a7bbc
aarne <aarne@cs.chalmers.se>
2007-06-04 14:14:04 +0000

    generating example file

---
+++ lib/resource-1.0/api/examples/MkLinks.hs
@@ -0,0 +1,20 @@
+src = "Constructors.html"
+tgt = "Cons.html"
+linkfile ex = "links/" ++ ex ++ ".txt"
+
+
+main = do
+  writeFile tgt ""
+  readFile src >>= (mapM_ mkLink . lines)
+
+mkLink line = case break (=="--") (words (takeWhile (/='#')line)) of
+  (fun : ":" : typ, _ : num : ex) ->
+    appendFile tgt ("\n" ++ takeInit line ++ " " ++ link ex typ num)
+  _ -> appendFile tgt ("\n" ++ line)
+ where
+   takeInit line = init (init (takeWhile (/='.') line))
+   link ex typ num = 
+     "<a href=\"" ++ linkfile (example typ num) ++ "\">" ++ unwords ex ++ "</a>"
+   example typ num = case reverse typ of
+     ";":val:_ -> "ex" ++ init num ++ "_" ++ val 
+     val:_     -> "ex" ++ init num ++ "_" ++ val 
