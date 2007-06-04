41abfd8092557c65833af3f2a7dc9ece2ec36749
aarne <aarne@cs.chalmers.se>
2007-06-04 16:14:04 +0200

    generating example file

--- lib/resource-1.0/api/examples/mkgfs.hs
+++ lib/resource-1.0/api/examples/mkgfs.hs
@@ -4,6 +4,7 @@ main = mkgfs
 
 src = "ExxI.gf"
 script = "exx.gfs"
+linkfile ex = "links/" ++ ex ++ ".txt"
 
 mkgfs = do
   writeFile script ""
@@ -11,7 +12,11 @@ mkgfs = do
 
 
 addLin s = case words s of
-  c@('e':'x':_):_ -> appendFile script ("l " ++ cc ++ "\n") where
+  c@('e':'x':_):"=":def -> do
+    appendFile script ("ps \"" ++ unwords (init def) ++ "\\n\\n\" | wf " ++ linkfile c ++ "\n")
+    appendFile script ("l -multi " ++ cc ++ " | af " ++ linkfile c ++ "\n")
+
+   where
     cc = case take 2 (reverse c) of
       "PV" -> "utt " ++ c
       _ -> c 
