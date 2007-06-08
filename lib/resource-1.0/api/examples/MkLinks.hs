ebbf576436bf57294381d41126d6241623bc8714
aarne <aarne@cs.chalmers.se>
2007-06-08 15:42:09 +0200

    fine-tuning api documentation

--- lib/resource-1.0/api/examples/MkLinks.hs
+++ lib/resource-1.0/api/examples/MkLinks.hs
@@ -1,3 +1,5 @@
+import Char (isDigit)
+
 src = "Constructors.html"
 tgt = "Cons.html"
 linkfile ex = "links/" ++ ex ++ ".txt"
@@ -8,13 +10,13 @@ main = do
   readFile src >>= (mapM_ mkLink . lines)
 
 mkLink line = case break (=="--") (words (takeWhile (/='#')line)) of
-  (fun : ":" : typ, _ : num : ex) ->
+  (fun : ":" : typ, _ : num : ex) | isDigit (head num) ->
     appendFile tgt ("\n" ++ takeInit line ++ " " ++ link ex typ num)
   _ -> appendFile tgt ("\n" ++ line)
  where
    takeInit line = init (init (takeWhile (/='.') line))
-   link ex typ num = 
-     "<a href=\"" ++ linkfile (example typ num) ++ "\">" ++ unwords ex ++ "</a>"
+   link ex typ num =
+        "<a href=\"" ++ linkfile (example typ num) ++ "\">" ++ unwords ex ++ "</a>"
    example typ num = case reverse typ of
      ";":val:_ -> "ex" ++ init num ++ "_" ++ val 
      val:_     -> "ex" ++ init num ++ "_" ++ val 
