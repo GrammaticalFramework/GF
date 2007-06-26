84e030d128c5168c7efb2aca1da0efe3395a57fa
bringert <bringert@cs.chalmers.se>
2007-06-26 15:09:48 +0000

    Oops, I had managed to remove the preprocessing step in makeSimpleRegular, which broke SLF and non-rec SRGS generation for grammars which need bottom-up filtering.

--- src/GF/Speech/CFGToFiniteState.hs
+++ src/GF/Speech/CFGToFiniteState.hs
@@ -62,7 +62,7 @@ cfgToFA opts s = minimize $ compileAutomaton start $ makeSimpleRegular opts s
   where start = getStartCatCF opts s
 
 makeSimpleRegular :: Options -> StateGrammar -> CFRules
-makeSimpleRegular opts s = makeRegular $ cfgToCFRules s
+makeSimpleRegular opts s = makeRegular $ preprocess $ cfgToCFRules s
   where start = getStartCatCF opts s
         preprocess = fix (topDownFilter start . bottomUpFilter)
                      . removeCycles
