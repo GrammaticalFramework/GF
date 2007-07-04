ada01966d34cfd1ae40002859d11f5d3e1f47623
bringert <bringert@cs.chalmers.se>
2007-07-04 14:49:01 +0200

    Build GF/Today.hs directly from Makefile. Include libdir.

--- src/tools/mktoday.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo 'module GF.Today (today,version) where' > GF/Today.hs
-echo '{-# NOINLINE today #-}' >> GF/Today.hs
-echo 'today,version :: String' >> GF/Today.hs
-echo 'today = "'`date`'"' >> GF/Today.hs
-echo 'version = "'$1'"' >> GF/Today.hs
-
