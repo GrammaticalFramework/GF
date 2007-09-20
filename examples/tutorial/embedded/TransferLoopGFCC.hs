549447458c9839790c79f192677d7e688236ca68
aarne <aarne@cs.chalmers.se>
2007-09-20 13:31:24 +0000

    math with gfcc

---
+++ examples/tutorial/embedded/TransferLoopGFCC.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import GF.Canon.GFCC.GFCCAPI
+import TransferDefGFCC (transfer)
+
+main :: IO () 
+main = do
+  gr <- file2grammar "math.gfcc"
+  loop (translate transfer gr)
+
+loop :: (String -> String) -> IO ()
+loop trans = do 
+  s <- getLine
+  if s == "quit" then putStrLn "bye" else do  
+    putStrLn $ trans s
+    loop trans
+
+translate :: (Tree -> Tree) -> MultiGrammar -> String -> String
+translate tr gr = unlines . map transLine . lines where
+  transLine s = case parseAllLang gr "Question" s of
+    (lg,t:_):_ -> linearize gr lg (tr t)
+    _ -> "NO PARSE"
+
