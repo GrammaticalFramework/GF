e91ea218fecb7511cf81de7d298372337e7401d1
aarne <aarne@cs.chalmers.se>
2007-09-21 11:15:14 +0200

    gfcc generation in gfc works for some grammars

--- src/GF/Devel/GFC.hs
+++ src/GF/Devel/GFC.hs
@@ -2,6 +2,7 @@ module Main where
 
 import GF.Devel.Compile
 import GF.Devel.GrammarToGFCC
+import GF.Devel.UseIO
 ---import GF.Devel.PrGrammar ---
 
 import System
@@ -13,9 +14,11 @@ main = do
     "-help":[] -> putStrLn "usage: gfc (--make) FILES"
     "--make":fs -> do
       gr <- batchCompile fs
-      --- putStrLn $ prGrammar gr
-      writeFile "a.gfcc" $ prGrammar2gfcc gr
-      putStrLn "Wrote file a.gfcc."
+      let name = justModuleName (last fs)
+      let (abs,gc) = prGrammar2gfcc name gr
+      let target = abs ++ ".gfcc"
+      writeFile target gc
+      putStrLn $ "wrote file " ++ target
     _ -> do
       mapM_ batchCompile (map return xx)
       putStrLn "Done."
