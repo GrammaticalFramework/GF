6fef03ed019e900a6382cac8564d9d2a8d10a107
aarne <aarne@cs.chalmers.se>
2007-11-28 11:58:47 +0000

    new definitions of term and judgement syntax

--- src/GF/Devel/Modules.hs
+++ src/GF/Devel/Modules.hs
@@ -1,10 +1,12 @@
 module GF.Devel.Modules where
 
-import GF.Grammar.Grammar
+import GF.Devel.Judgements
+import GF.Devel.Terms
 import GF.Infra.Ident
 
 import GF.Data.Operations
 
+import Control.Monad
 import Data.Map
 
 
@@ -45,23 +47,45 @@ data MInclude =
   | MIExcept [Ident]
   | MIOnly [Ident]
 
-data Judgement = Judgement {
-  jform :: JudgementForm,  -- cat         fun        oper    param
-  jtype :: Type,           -- context     type       type    type
-  jdef  :: Term,           -- lindef      def        -       values
-  jlin  :: Term,           -- lincat      lin        def     constructors
-  jprintname :: Term       -- printname   printname  -       -
-  }
 
-data JudgementForm =
-    JCat
-  | JFun
-  | JOper
-  | JParam
+-- look up fields for a constant in a grammar
 
+lookupJField :: (Judgement -> a) -> GF -> Ident -> Ident -> Err a
+lookupJField field gf m c = do
+  j <- lookupJudgement gf m c
+  return $ field j
+
+lookupJForm :: GF -> Ident -> Ident -> Err JudgementForm
+lookupJForm = lookupJField jform
+
+-- the following don't (need to) check that the jment form is adequate
+ 
+lookupCatContext :: GF -> Ident -> Ident -> Err Context
+lookupCatContext gf m c = do
+  ty <- lookupJField jtype gf m c
+  return [] ---- context of ty
+
+lookupFunType :: GF -> Ident -> Ident -> Err Term
+lookupFunType = lookupJField jtype 
+
+lookupLin :: GF -> Ident -> Ident -> Err Term
+lookupLin = lookupJField jlin
+
+lookupLincat :: GF -> Ident -> Ident -> Err Term
+lookupLincat = lookupJField jlin
+
+lookupParamValues :: GF -> Ident -> Ident -> Err [Term]
+lookupParamValues gf m c = do
+  j <- lookupJudgement gf m c
+  case jdef j of
+    V _ ts -> return ts
+    _ -> raise "no parameter values"
+
+-- infrastructure for lookup
+    
 lookupIdent :: GF -> Ident -> Ident -> Err (Either Judgement Ident)
 lookupIdent gf m c = do
-  mo <- maybe (Bad "module not found") return $ mlookup m (gfmodules gf)
+  mo <- maybe (raise "module not found") return $ mlookup m (gfmodules gf)
   maybe (Bad "constant not found") return $ mlookup c (mjments mo)
 
 lookupJudgement :: GF -> Ident -> Ident -> Err Judgement
