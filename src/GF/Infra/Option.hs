273848d1711aa23f4d309b95d04b1d97ddcad22d
bjorn <bjorn@bringert.net>
2008-09-25 13:49:00 +0200

    Added --output-format=regular.

--- src/GF/Infra/Option.hs
+++ src/GF/Infra/Option.hs
@@ -87,6 +87,7 @@ data OutputFormat = FmtPGF
                   | FmtProlog
                   | FmtProlog_Abs
                   | FmtBNF
+                  | FmtRegular
                   | FmtFCFG
                   | FmtSRGS_XML
                   | FmtSRGS_XML_NonRec
@@ -456,6 +457,7 @@ outputFormats =
      ("prolog",       FmtProlog),
      ("prolog_abs",   FmtProlog_Abs),
      ("bnf",          FmtBNF),
+     ("regular",      FmtRegular),
      ("fcfg",         FmtFCFG),
      ("srgs_xml",     FmtSRGS_XML),
      ("srgs_xml_nonrec",     FmtSRGS_XML_NonRec),
