353bea7b8a48ef86eff3c2e734d3f26916b39d91
krasimir <krasimir@chalmers.se>
2008-10-25 22:25:09 +0200

    add cp1252. usefull for Swedish and German

--- src/GF/Text/Coding.hs
+++ src/GF/Text/Coding.hs
@@ -2,13 +2,16 @@ module GF.Text.Coding where
 
 import GF.Text.UTF8
 import GF.Text.CP1251
+import GF.Text.CP1252
 
 encodeUnicode e = case e of
   "utf8"   -> encodeUTF8
   "cp1251" -> encodeCP1251
+  "cp1252" -> decodeCP1252
   _        -> id
 
 decodeUnicode e = case e of
   "utf8"   -> decodeUTF8
   "cp1251" -> decodeCP1251
+  "cp1252" -> decodeCP1252
   _        -> id
