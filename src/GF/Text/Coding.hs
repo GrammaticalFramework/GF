79d51c4479125bbc055df876d10c42411b7e46da
krasimir <krasimir@chalmers.se>
2008-12-17 23:53:12 +0000

    added codepage 1250 i.e. central european. Useful for  Polish, Czech, Slovak, Hungarian, Slovene, Bosnian, Croatian, Serbian, Romanian and Albanian

--- src/GF/Text/Coding.hs
+++ src/GF/Text/Coding.hs
@@ -1,17 +1,20 @@
 module GF.Text.Coding where
 
 import GF.Text.UTF8
+import GF.Text.CP1250
 import GF.Text.CP1251
 import GF.Text.CP1252
 
 encodeUnicode e = case e of
   "utf8"   -> encodeUTF8
+  "cp1250" -> encodeCP1250
   "cp1251" -> encodeCP1251
   "cp1252" -> encodeCP1252
   _        -> id
 
 decodeUnicode e = case e of
   "utf8"   -> decodeUTF8
+  "cp1250" -> decodeCP1250
   "cp1251" -> decodeCP1251
   "cp1252" -> decodeCP1252
   _        -> id
