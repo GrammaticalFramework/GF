fcedc6f6e74d0d8bd9f34e03b7319b1353db39aa
krasimir <krasimir@chalmers.se>
2009-01-25 18:12:56 +0200

    add Fridge-compile.bat

---
+++ src/server/gwt/Fridge-compile.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set APPDIR=.
+set GWT_DIR=c:\gwt-windows-1.5.2
+set GWT_CLASSPATH="%GWT_DIR%\gwt-user.jar;%GWT_DIR%\gwt-dev-windows.jar"
+
+set LIBS=%APPDIR%\lib\gwt-dnd-2.5.6.jar
+
+java %GWT_JAVA_OPTS% -Xmx256M -cp "%APPDIR%\src;%APPDIR%\bin;%LIBS%;%GWT_CLASSPATH%" com.google.gwt.dev.GWTCompiler -out "%APPDIR%\www\fridge" se.chalmers.cs.gf.gwt.FridgeApp
