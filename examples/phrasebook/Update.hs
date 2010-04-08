6aa9363b530695ec3098b3baae97e41b98202e88
aarne <aarne@chalmers.se>
2010-04-08 11:52:44 +0000

    started a script and format for updating a set of gf files

---
+++ examples/phrasebook/Update.hs
@@ -0,0 +1,23 @@
+import System
+
+main = do
+  file:_  <- getArgs
+  updates <- readFile file >= return . readUpdates
+  mapM_ (doUpdate file) updates
+  return ()
+
+type Update = (FilePath, [String])
+
+readUpdates :: String -> [Update]
+readUpdates s = []
+
+doUpdate :: FilePath -> Update -> IO ()
+doUpdate src (target,ls) = do
+  s <- readFile target 
+  let beg = dropLastBracket s
+  let tmp = tmpFile target
+  writeFile tmp beg
+  appendFile tmp $ unlines [(line ++ "-- UPDATE FROM " ++ src) | line <- ls]
+  appendFile tmp "\n}\n"
+
+dropLastBracket s = s
