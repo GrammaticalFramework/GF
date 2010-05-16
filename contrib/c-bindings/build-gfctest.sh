87a8d91d3be0c2298fa3eb42c9b057dacc291e82
kevin.kofler <kevin.kofler@chello.at>
2010-05-16 21:12:01 +0000

    Build Query.pgf in build-gfctest.sh and look for it in the current directory

--- contrib/c-bindings/build-gfctest.sh
+++ contrib/c-bindings/build-gfctest.sh
@@ -16,5 +16,6 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, see <http://www.gnu.org/licenses/>.
 
+gf --make ../../examples/tutorial/embedded/QueryEng.gf &&
 ghc --make -fglasgow-exts -O2 -no-hs-main $* -c PGFFFI.hs &&
 ghc --make -fglasgow-exts -O2 -no-hs-main $* gfctest.c gf_lexing.c PGFFFI.hs -o gfctest
