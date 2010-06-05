2e4c7dd5350900b97fb883c5823b318ed929743e
jordi.saludes <jordi.saludes@upc.edu>
2010-06-05 13:01:09 +0200

    Fixed c-bindings gfctest

--- contrib/c-bindings/build-gfctest.sh
+++ contrib/c-bindings/build-gfctest.sh
@@ -15,7 +15,9 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, see <http://www.gnu.org/licenses/>.
-
-gf --make ../../examples/tutorial/embedded/QueryEng.gf &&
-ghc --make -fglasgow-exts -O2 -no-hs-main $* -c PGFFFI.hs &&
-ghc --make -fglasgow-exts -O2 -no-hs-main $* gfctest.c gf_lexing.c PGFFFI.hs -o gfctest
+gf=../../dist/build/gf/gf
+src=../../src
+import=-i$src/runtime/haskell:$src/compiler
+$gf --make ../../examples/tutorial/embedded/QueryEng.gf &&
+ghc $import --make -fglasgow-exts -O2 -no-hs-main $* -c PGFFFI.hs &&
+ghc $import --make -fglasgow-exts -O2 -no-hs-main $* gfctest.c  PGFFFI.hs -o gfctest # gf_lexing.c
