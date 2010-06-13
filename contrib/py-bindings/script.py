e18d121315cfa04a5ae61046cecc6cc73017c395
jordi.saludes <jordi.saludes@upc.edu>
2010-06-13 17:07:49 +0000

    DEBUG mode added.

--- contrib/py-bindings/script.py
+++ contrib/py-bindings/script.py
@@ -3,7 +3,8 @@ import gf
 query = gf.read_pgf("Query.pgf")
 lang = gf.read_language('QueryEng')
 cat = query.startcat()
-print 'start category:',cat
+print 'start category:', cat
+print 'language is:', lang
 lexed = "is 2 prime"
 print "Parsing '%s':" % lexed
 for e in query.parse(lexed, lang):
