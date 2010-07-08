1cb3cfb4c261ec82386955e29e49e991d52cf726
krasimir <krasimir@chalmers.se>
2010-07-08 09:55:09 +0000

    change the package name of the GWT Apps to org.grammaticalframework.ui.gwt

--- src/server/gwt/src/org/grammaticalframework/ui/gwt/client/MyListBox.java
+++ src/server/gwt/src/org/grammaticalframework/ui/gwt/client/MyListBox.java
@@ -1,4 +1,4 @@
-package se.chalmers.cs.gf.gwt.client;
+package org.grammaticalframework.ui.gwt.client;
 
 import java.util.Collection;
 
--- src/server/gwt/src/org/grammaticalframework/ui/gwt/client/StatusPopup.java
+++ src/server/gwt/src/org/grammaticalframework/ui/gwt/client/StatusPopup.java
@@ -1,4 +1,4 @@
-package se.chalmers.cs.gf.gwt.client;
+package org.grammaticalframework.ui.gwt.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.Label;
