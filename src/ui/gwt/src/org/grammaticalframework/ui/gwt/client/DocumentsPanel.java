33de2ce9deaf496a724dfe2ef837bb7593530650
krasimir <krasimir@chalmers.se>
2010-10-07 16:09:08 +0200

    dummy UI for documents saving/loading in the editor

---
+++ src/ui/gwt/src/org/grammaticalframework/ui/gwt/client/DocumentsPanel.java
@@ -0,0 +1,56 @@
+package org.grammaticalframework.ui.gwt.client;
+
+import java.util.*;
+import com.google.gwt.user.client.*;
+import com.google.gwt.user.client.ui.*;
+import com.google.gwt.http.client.*;
+import com.google.gwt.xml.client.*;
+import com.google.gwt.event.logical.shared.*;
+import com.google.gwt.event.dom.client.*;
+
+public class DocumentsPanel extends Composite {
+
+	private PGFWrapper pgf;
+	private FlexTable table;
+
+	public DocumentsPanel(PGFWrapper pgf) {
+		this.pgf = pgf;
+
+		VerticalPanel documentsPanel = new VerticalPanel();
+		documentsPanel.setStylePrimaryName("my-DocumentsFrame");
+		
+		Label header = new Label("Documents");
+		header.setStylePrimaryName("my-DocumentsHeader");
+		documentsPanel.add(header);
+		
+		table = new FlexTable();
+		table.setStylePrimaryName("my-DocumentsTable");
+		documentsPanel.add(table);
+
+		addRow(0, "Test0");
+		addRow(1, "Test1");
+		addRow(2, "Test2");
+		addRow(3, "Test3");
+		
+		table.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				HTMLTable.Cell cell = table.getCellForEvent(event);
+				if (cell != null) {
+					int row = cell.getRowIndex();
+					selectRow(row);
+				}
+			}
+		});
+
+		initWidget(documentsPanel);
+		setStylePrimaryName("my-DocumentsPanel");
+	}
+	
+	private void addRow(int row, String text) {
+		table.setText(row, 0, text);
+		table.getRowFormatter().addStyleName(row, "row");
+	}
+	
+	private void selectRow(int row) {
+	}	
+}
