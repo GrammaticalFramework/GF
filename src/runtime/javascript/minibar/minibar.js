a8134994ab40de6c2ba5d3fcadd984b2d14dccae
hallgren <hallgren@chalmers.se>
2010-12-01 16:28:27 +0000

    Let minibar keep its own copy of tree-btn.png

--- src/runtime/javascript/minibar/minibar.js
+++ src/runtime/javascript/minibar/minibar.js
@@ -1,9 +1,6 @@
 // minibar.js, assumes that support.js has also been loaded
 
-//var default_server="http://www.grammaticalframework.org:41296"
-var default_server="http://localhost:41296"
-
-var tree_icon=default_server+"/translate/se.chalmers.cs.gf.gwt.TranslateApp/tree-btn.png";
+var tree_icon="tree-btn.png";
 
 /*
 // This is essentially what happens when you call start_minibar:
