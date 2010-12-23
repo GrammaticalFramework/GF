5f16e3134bee9dc93bda7ad2eb9ebeb9ee86ef5f
hallgren <hallgren@chalmers.se>
2010-12-23 12:40:04 +0000

    build-binary-dsit.sh: make paths relative to /usr/local in the final tar file.

--- build-binary-dist.sh
+++ build-binary-dist.sh
@@ -5,6 +5,7 @@
 ### the Haskell Platform, version 2010.1.0.0 or 2010.2.0.0
 
 destdir=/tmp/gf-build-binary-dist  # assemble binary dist here
+prefix=/usr/local                  # where to install
 targz=gf-bin.tar.gz                # the final tar file, should be renamed 
 langs=""                           # which languages?
 #langs="langs=-Pol" # temporary problem with Polish, omit it
@@ -14,7 +15,7 @@ set -x                             # print commands before exuting them
 
 cabal install    # gf needs to be installed before building gf-server below
 
-runhaskell Setup.hs configure --user --prefix /usr/local
+runhaskell Setup.hs configure --user --prefix $prefix
 runhaskell Setup.hs build $langs
 runhaskell Setup.hs copy --destdir=$destdir $langs
 
@@ -25,10 +26,10 @@ cd src/server
 ## the easiest way to install them:
 #cabal install
 
-runhaskell Setup.hs configure --user --prefix /usr/local
+runhaskell Setup.hs configure --user --prefix $prefix
 runhaskell Setup.hs build
 runhaskell Setup.hs copy --destdir=$destdir
 )
-tar -C $destdir -zcf $targz .
+tar -C $destdir/$prefix -zcf $targz .
 echo "Created $targz, rename it to something more informative"
 rm -r $destdir
