f847344de8b0b378b60f4646c8037abd456e4faf
aarne <aarne@chalmers.se>
2011-10-03 14:17:56 +0000

    demo grammar for the resource: mini/MiniGrammar. Generated automatically from MiniGrammarEng by make. Will replace the old demo/ grammars as one that illustrates the grammar structures while being lighter than Lang and generating less junk.

---
+++ lib/src/mini/MkMini.hs
@@ -0,0 +1,18 @@
+src = "Eng"
+langs = ["Afr","Bul","Cat","Dan","Dut","Fin","Fre","Ger","Ita",
+         "Nep","Nor","Pes","Pol","Pnb","Ron","Rus","Spa","Swe","Urd"]
+
+file lng = "MiniGrammar" ++ lng ++ ".gf"
+
+main = do
+  s <- readFile (file src)
+  mapM_ (mkMiniFile s) langs
+
+mkMiniFile s lng =
+  writeFile (file lng) (mkMini lng s)
+
+mkMini lng s = case s of
+  'E':'n':'g':cs -> lng ++ mkMini lng cs
+  c:cs -> c : mkMini lng cs
+  _ -> s
+
