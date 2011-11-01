f46593559a991d212da420cba780a138fb608afe
hallgren <hallgren@chalmers.se>
2011-11-01 13:45:12 +0000

    Remove configuration flag cclazy

--- src/compiler/GF/Compile/Compute/Concrete.hs
+++ src/compiler/GF/Compile/Compute/Concrete.hs
@@ -1,7 +1,3 @@
-{-# LANGUAGE CPP #-}
 module GF.Compile.Compute.Concrete(module M) where
-#ifdef CC_LAZY
-import GF.Compile.Compute.ConcreteLazy as M -- New, experimental
-#else
-import GF.Compile.Compute.ConcreteStrict as M -- Old, trusted
-#endif
+import GF.Compile.Compute.ConcreteLazy as M -- New
+--import GF.Compile.Compute.ConcreteStrict as M -- Old, inefficient
