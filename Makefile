08f17194f8603b1e20c13e2a44e4506e4c649373
aarne <aarne@chalmers.se>
2012-02-25 09:53:09 +0000

    Hin and Snd in MiniGrammar demo

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: all build install doc clean sdist
+.PHONY: all build install doc clean sdist gf
 
 all: build
 
--- lib/src/mini/MkMini.hs
+++ lib/src/mini/MkMini.hs
@@ -1,10 +1,10 @@
 src = "Eng"
 langs = [
-  "Afr", "Bul", "Cat","Dan","Dut",
-  "Fin","Fre","Ger","Ita", "Lav",
-  "Nep","Nor","Pes","Pol","Pnb",
-  "Ron","Rus","Spa","Swe","Tha",
-  "Urd"
+  "Afr","Bul","Cat","Dan","Dut",
+  "Fin","Fre","Ger","Hin","Ita",
+  "Lav","Nep","Nor","Pes","Pol",
+  "Pnb","Ron","Rus","Snd","Spa",
+  "Swe","Tha","Urd"
   ]
 
 file lng = "MiniGrammar" ++ lng ++ ".gf"
