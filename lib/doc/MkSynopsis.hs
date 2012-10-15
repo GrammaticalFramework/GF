3cf42aaca8c32558e0dd3c5122dd8c05f6bbc541
aarne <aarne@chalmers.se>
2012-10-15 12:00:52 +0200

    enable full language list in MkSynopsis

--- lib/doc/MkSynopsis.hs
+++ lib/doc/MkSynopsis.hs
@@ -17,8 +17,8 @@ revealedLang = "Eng"
 
 -- all languages shown
 apiExxFiles = ["api-examples-" ++ lang ++ ".txt" | lang <- words 
-   "Eng Chi" 
---  "Afr Bul Cat Chi Dan Dut Eng Fin Fre Ger Hin Ita Jpn Lav Nep Nor Pes Pnb Pol Ron Rus Snd Spa Swe Tha Urd"
+--   "Eng Chi" 
+  "Afr Bul Cat Chi Dan Dut Eng Fin Fre Ger Hin Ita Jpn Lav Nep Nor Pes Pnb Pol Ron Rus Snd Spa Swe Tha Urd"
    ]
 
 main = do
