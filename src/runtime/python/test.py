81428c768c51218c325f77d42c5601b0c1c49550
kr.angelov <kr.angelov@gmail.com>
2012-12-13 16:19:56 +0000

    added a simple test for the Python binding

---
+++ src/runtime/python/test.py
@@ -0,0 +1,23 @@
+import sys
+import pgf
+
+sys.stdout.write("loading...")
+sys.stdout.flush();
+gr = pgf.readPGF("../../../treebanks/PennTreebank/ParseEngAbs.pgf")
+sys.stdout.write("\n")
+
+we = pgf.readExpr("UttImpSg PPos (ImpVP (UseV try_V))")
+print gr.languages["ParseEng"].linearize(we)
+
+sys.stdout.write("start cat: "+gr.startCat+"\n\n")
+
+while True:
+	sys.stdout.write("> ")
+	line = sys.stdin.readline();
+	if line == '':
+		sys.stdout.write("\n")
+		break;
+
+	for e in gr.languages["ParseEng"].parse(gr.startCat,line):
+		print e
+		print gr.languages["ParseEngBul"].linearize(e)
