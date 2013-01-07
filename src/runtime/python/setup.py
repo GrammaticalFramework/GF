c45addac630b812ad94053610986a7537bab9ee3
kr.angelov <kr.angelov@gmail.com>
2013-01-07 18:11:12 +0200

    now the Python binding has an alternative representation for abstract trees which is composed of Python objects. The new representation is not integrated with the core runtime yet

--- src/runtime/python/setup.py
+++ src/runtime/python/setup.py
@@ -1,11 +1,15 @@
 from distutils.core import setup, Extension
 
-pgf_module = Extension('pgf',
-                       sources = ['pypgf.c'],
+pgf_module = Extension('pgf.binding',
+                       sources = ['pgf/binding.c'],
                        extra_compile_args = ['-std=c99'],
                        libraries = ['gu', 'pgf'])
 
 setup (name = 'pgf',
        version = '1.0',
-       description = 'This is binding to the PGF engine',
+       description = 'A binding to the PGF engine',
+       author='Krasimir Angelov',
+       author_email='kr.angelov@gmail.com',
+       license='BSD',
+       packages=['pgf'],
        ext_modules = [pgf_module])
