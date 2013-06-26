67872578c9541cd63184ba8e7c3dfab1294939ba
kr.angelov <kr.angelov@gmail.com>
2013-06-26 09:08:47 +0000

    forgot to add jit.h

---
+++ src/runtime/c/pgf/jit.h
@@ -0,0 +1,16 @@
+#ifndef PGF_JIT_H_
+#define PGF_JIT_H_
+
+typedef struct PgfJitState PgfJitState;
+
+PgfJitState*
+pgf_jit_init(GuPool* tmp_pool, GuPool* pool);
+
+void
+pgf_jit_done(PgfJitState* state, PgfAbstr* abstr);
+
+void
+pgf_jit_predicate(PgfJitState* state, 
+                  PgfCIdMap* abscats, PgfAbsCat* abscat);
+
+#endif
