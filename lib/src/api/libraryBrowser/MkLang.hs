d0f7df60d325af2a5e3b6ffeeda377b8524896b7
john.j.camilleri <john.j.camilleri@chalmers.se>
2013-07-26 07:41:20 +0000

    lib/src/api/libraryBrowser/MkLang.hs - allow multiple arguments

--- lib/src/api/libraryBrowser/MkLang.hs
+++ lib/src/api/libraryBrowser/MkLang.hs
@@ -1,16 +1,18 @@
-
 import System.Environment (getArgs)
 import Data.List.Utils (replace)
-
-name = "LibraryBrowserEng.gf"
 
 main = do
   a <- getArgs
   case a of
-    lang:_ -> do
-      contents <- readFile name
-      let name'     = replace "Eng" lang name
-      let contents' = replace "Eng" lang contents
-      writeFile name' contents'
-      putStrLn $ "Wrote " ++ name'
-    _ -> print "You need to provide a language code"
+    [] -> print "You need to provide a language code"
+    langs -> mapM_ makeOne langs
+
+name = "LibraryBrowserEng.gf"
+
+makeOne "Eng" = putStrLn "Skipping Eng"
+makeOne lang  = do
+  contents <- readFile name
+  let name'     = replace "Eng" lang name
+  let contents' = replace "Eng" lang contents
+  writeFile name' contents'
+  putStrLn $ "Wrote " ++ name'
