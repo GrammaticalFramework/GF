9d98e773bbab5fbd51ef24612377aebf6c04180a
kr.angelov <kr.angelov@gmail.com>
2013-08-28 00:54:10 +0200

    added method for best first parsing in the Java binding

--- src/runtime/java/org/grammaticalframework/pgf/Concr.java
+++ src/runtime/java/org/grammaticalframework/pgf/Concr.java
@@ -9,7 +9,16 @@ public class Concr {
 	public Iterable<ExprProb> parse(String startCat, String s) {
 		return new Parser(this, startCat, s);
 	}
-	
+
+	public Expr parseBest(String startCat, String s) {
+		Iterator<ExprProb> iter = Parser.parse(this, startCat, s);
+		if (iter.hasNext()) {
+			return iter.next().getExpr();
+		} else {
+			return null;
+		}
+	}
+
 	public native String linearize(Expr expr);
 
 	//////////////////////////////////////////////////////////////////
