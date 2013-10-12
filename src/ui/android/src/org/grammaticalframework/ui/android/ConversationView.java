add86fcf1d440b07a3f129480e9e4bbf0ef719a4
kr.angelov <kr.angelov@gmail.com>
2013-10-12 12:28:23 +0000

    add keyboard input in the android app

--- src/ui/android/src/org/grammaticalframework/ui/android/ConversationView.java
+++ src/ui/android/src/org/grammaticalframework/ui/android/ConversationView.java
@@ -5,9 +5,14 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.view.KeyEvent;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 public class ConversationView extends ScrollView {
 
@@ -15,7 +20,8 @@ public class ConversationView extends ScrollView {
 
     private ViewGroup mContent;
     
-    private OnWordSelectedListener mListener;
+    private OnWordSelectedListener mWordListener;
+    private ASR.Listener mSpeechListener;
 
     public ConversationView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
@@ -49,6 +55,47 @@ public class ConversationView extends ScrollView {
         });
     }
 
+    public void addInputBox() {
+        final View view = 
+        	mInflater.inflate(R.layout.input_box, mContent, false);
+        EditText edittext = (EditText) view.findViewById(R.id.input_text);
+        edittext.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                	InputMethodManager inputMethodManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    if (inputMethodManager != null) {
+                        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+                    }
+                }
+            }
+        });
+        edittext.setOnEditorActionListener(new OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((actionId & EditorInfo.IME_MASK_ACTION) != 0) {
+                	CharSequence text = v.getText();
+                	mContent.removeView(view);
+                	addFirstPersonUtterance("...");
+                	InputMethodManager inputMethodManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    if (inputMethodManager != null) {
+                        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0);
+                    }
+                    if (mSpeechListener != null)
+                    	mSpeechListener.onSpeechInput(text.toString());
+                    return true;
+                }
+                return false;
+            }
+        });
+        mContent.addView(view);
+        post(new Runnable() {
+            public void run() {
+                fullScroll(FOCUS_DOWN);
+            }
+        });
+    }
+
     public void addSecondPersonUtterance(CharSequence text) {
         TextView view = (TextView) 
         	mInflater.inflate(R.layout.second_person_utterance, mContent, false);
@@ -68,7 +115,7 @@ public class ConversationView extends ScrollView {
             TextView textview = (TextView) view.findViewById(R.id.text);
             textview.setText(text);
             
-            if (lexicon != null && mListener != null) {
+            if (lexicon != null && mWordListener != null) {
             	ImageView showWordButton = (ImageView) view.findViewById(R.id.show_word);
             	showWordButton.setVisibility(VISIBLE);
 
@@ -76,10 +123,12 @@ public class ConversationView extends ScrollView {
     	        showWordButton.setOnClickListener(new OnClickListener() {
     	        	@Override
     	        	public void onClick(View v) {
-    	        		TextView textview = (TextView)
+    	        		if (mWordListener != null) {
+    	        			TextView textview = (TextView)
     	        				((View) v.getParent()).findViewById(R.id.text);
-    	        		CharSequence text = textview.getText();
-    	        		mListener.onWordSelected(text, lexicon2);
+    	        			CharSequence text = textview.getText();
+    	        			mWordListener.onWordSelected(text, lexicon2);
+    	        		}
     	        	}
     	        });    	        
             }
@@ -87,7 +136,11 @@ public class ConversationView extends ScrollView {
     }
 
     public void setOnWordSelectedListener(OnWordSelectedListener listener) {
-    	mListener = listener;
+    	mWordListener = listener;
+    }
+    
+    public void setSpeechInputListener(ASR.Listener listener) {
+    	mSpeechListener = listener;
     }
     
     public interface OnWordSelectedListener {
