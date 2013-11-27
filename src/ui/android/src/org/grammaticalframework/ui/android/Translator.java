38b4a88c2b46e4ab6e660869342576eec72bb1cd
kr.angelov <kr.angelov@gmail.com>
2013-11-27 12:37:55 +0000

    minor tweaks in the Android UI

--- src/ui/android/src/org/grammaticalframework/ui/android/Translator.java
+++ src/ui/android/src/org/grammaticalframework/ui/android/Translator.java
@@ -127,7 +127,7 @@ public class Translator {
             return output;
         } catch (ParseError e) {
             Log.e(TAG, "Parse error: " + e);
-            return "parse error: " + e.getMessage(); // TODO: no no no
+            return "parse error: " + e.getMessage();
         }
     }
 
