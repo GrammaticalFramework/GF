a1da853486236b19780504446091fe3a4ed9c3e4
hallgren <hallgren@chalmers.se>
2014-02-19 15:15:14 +0100

    src/www/js/langcode.js: functions for converting language codes

--- src/www/translator/translator.js
+++ src/www/translator/translator.js
@@ -1036,32 +1036,6 @@ function show_translation(o) {
 
 /* --- Auxiliary functions -------------------------------------------------- */
 
-function lang1(namecode2) {
-    function lang(code,name,code2) {return {code:code, name:name, code2:code2}}
-    var nc=namecode2.split(":");
-    var name=nc[0]
-    var ws=name.split("/");
-    var code2=nc.length>1 ? nc[1] : ""
-    return ws.length==1 ? lang(name.substr(0,3),name,code2)
-	                : lang(ws[0],ws[1],code2);
-}
-
-// Language names and ISO-639 codes (both 3-letter and 2-letter codes)
-// See http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
-var languages = // [ISO-639-2 code "/"] language name ":" ISO 639-1 code
-    map(lang1,"Amharic:am Arabic:ar Bulgarian:bg Catalan:ca Chinese:zh Danish:da Dutch:nl English:en Finnish:fi French:fr German:de Greek:el Hindi:hi Ina/Interlingua:ia Italian:it Jpn/Japanese:ja Latin:la Norwegian:nb Polish:pl Ron/Romanian:ro Russian:ru Spanish:es Swedish:sv Thai:th Turkish:tr Urdu:ur".split(" "));
-
-var langname={};
-var langcode2={}
-var langcode3={}
-for(var i in languages) {
-    langname[languages[i].code]=languages[i].name
-    langcode2[languages[i].code]=languages[i].code2
-    langcode3[languages[i].code2]=languages[i].code
-}
-function concname(code) { return langname[code] || code; }
-function alangcode(code) { return langcode2[code] || code; }
-
 // Collect alternative texts in the output from PGF service translate command
 function collect_texts(ts) {
     var list=[]
