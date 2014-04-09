2c1002b9dd3b966159bfa23e0351e4d83a8ea3f5
aarne <aarne@chalmers.se>
2014-04-09 16:01:06 +0200

    more passives in App. Import changes are now produced with MkApp.hs, which copies the exclude list from App.gf to the concrete syntax files.

---
+++ examples/app/MkApp.hs
@@ -0,0 +1,19 @@
+-- update the import list of every language
+
+langs = words "Bul Chi Eng Fin Fre Ger Hin Ita Spa Swe"
+appCnc lang = "App" ++ lang ++ ".gf"
+
+appAbs = "App.gf"
+
+main = do
+  imports <- readFile appAbs >>= return . getImports
+  mapM_ (\f -> readFile f >>= writeFile ("tmp/" ++ f) . (putImports imports)) (map appCnc langs)
+
+getImports = takeWhile (/= ']') . tail . dropWhile (/='[')
+
+putImports i s = 
+  let
+    (s1,_:s2) = span (/='[') s
+    (_,   s3) = span (/=']') s2
+  in s1 ++ "[" ++ i ++ s3
+
