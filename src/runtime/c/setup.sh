bbeca32a437af9a30816e8a2f020498d3f332a39
hallgren <hallgren@chalmers.se>
2014-06-16 23:28:12 +0000

    src/runtime/c/setup.sh: executable version of INSTALL, used from debian/rules

---
+++ src/runtime/c/setup.sh
@@ -0,0 +1,21 @@
+
+# Executable version of INSTALL
+
+command="$1"
+shift
+
+case "$command" in
+  configure)
+    [ `uname` != Darwin ] || glibtoolize
+    autoreconf -i
+    ./configure "$@"
+    ;;
+  build)
+    make "$@"
+    ;;
+  copy|install)
+    make install "$@"
+    ;;
+  *)
+    echo "Usage: setup.sh [configure|build|copy|install] ..."
+esac
