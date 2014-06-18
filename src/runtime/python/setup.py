842b7d23451ebad2e8004a64fadf7db8f2aaae0a
hallgren <hallgren@chalmers.se>
2014-06-18 18:09:46 +0200

    build-binary-dist.sh: updated to include the Python binding to the C run-time

--- src/runtime/python/setup.py
+++ src/runtime/python/setup.py
@@ -1,8 +1,18 @@
 from distutils.core import setup, Extension
+import os
+
+includes = os.getenv('EXTRA_INCLUDE_DIRS','').split(':')
+if includes==['']:
+    includes=[]
+libraries = os.getenv('EXTRA_LIB_DIRS','').split(':')
+if libraries==['']:
+    libraries=[]
 
 pgf_module = Extension('pgf',
                        sources = ['pypgf.c'],
                        extra_compile_args = ['-std=c99'],
+                       include_dirs = includes,
+                       library_dirs = libraries,
                        libraries = ['gu', 'pgf'])
 
 setup (name = 'pgf',
