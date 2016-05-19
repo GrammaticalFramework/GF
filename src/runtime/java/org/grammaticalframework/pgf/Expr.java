12c94c00f0bf417438cbc382fbb9fd8458f51b13
krasimir <krasimir@chalmers.se>
2016-05-19 22:48:47 +0200

    in case of null argument it is more intuitive to report the argument index by counting from 1.

--- src/runtime/java/org/grammaticalframework/pgf/Expr.java
+++ src/runtime/java/org/grammaticalframework/pgf/Expr.java
@@ -40,7 +40,7 @@ public class Expr implements Serializable {
 				throw new IllegalArgumentException("fun == null");
 			for (int i = 0; i < args.length; i++) {
 				if (args[i] == null)
-					throw new IllegalArgumentException("the "+i+"th argument is null");
+					throw new IllegalArgumentException("the "+(i+1)+"th argument is null");
 			}
 
 			this.pool   = new Pool();
