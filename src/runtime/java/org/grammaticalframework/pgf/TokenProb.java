409bff8e7054fca6fd93859fc978e7cc670294f9
krasimir <krasimir@chalmers.se>
2016-05-25 14:39:51 +0200

    the Java API now also has access to the token's category while doing completion

--- src/runtime/java/org/grammaticalframework/pgf/TokenProb.java
+++ src/runtime/java/org/grammaticalframework/pgf/TokenProb.java
@@ -3,11 +3,18 @@ package org.grammaticalframework.pgf;
 /** Simply a pair of an expression and a probability value. */
 public class TokenProb {
 	private String tok;
+	private String cat;
 	private double prob;
 
-	public TokenProb(String tok, double prob) {
-		this.tok  = tok;
+	public TokenProb(double prob, String tok, String cat) {
 		this.prob = prob;
+		this.tok  = tok;
+		this.cat  = cat;		
+	}
+
+	/** Returns the negative logarithmic probability. */
+	public double getProb() {
+		return prob;
 	}
 
 	/** Returns the token. */
@@ -15,8 +22,8 @@ public class TokenProb {
 		return tok;
 	}
 
-	/** Returns the negative logarithmic probability. */
-	public double getProb() {
-		return prob;
+	/** Returns the category from which this word was predicted. */
+	public String getCategory() {
+		return cat;
 	}
 }
