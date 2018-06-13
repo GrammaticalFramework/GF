f129f5fb5db2af39b0087ac573505bbcd437912d
John J. Camilleri <john@digitalgrammars.com>
2018-06-13 11:33:37 +0200

    Be consistent with quotes and vars in split shell script

--- split/run.sh
+++ split/run.sh
@@ -18,7 +18,7 @@ COMMIT_PREFIX="[GF Split] "
 # https://stackoverflow.com/a/246128/98600
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR="${THISDIR}/../../GF-SPLIT/"
-mkdir -p $DIR
+mkdir -p "$DIR"
 
 # Repository names
 REP_PRISTINE="GF-pristine"
@@ -29,22 +29,22 @@ REP_ARCHIVE="gf-archive"
 # --- Setting up --------------------------------------------------------------
 
 # Make a brand new clone which is copied for each sub repo
-cd $DIR
-echo "# ${REP_PRISTINE}"
-if [ -d $REP_PRISTINE ]; then
+cd "$DIR"
+echo "# $REP_PRISTINE"
+if [ -d "$REP_PRISTINE" ]; then
   echo "Pulling..."
-  cd $REP_PRISTINE
+  cd "$REP_PRISTINE"
   git pull --quiet
   echo "Cleaning..."
   git clean -x --force
   cd ..
 else
   echo "Cloning..."
-  git clone git@github.com:GrammaticalFramework/GF.git $REP_PRISTINE
+  git clone git@github.com:GrammaticalFramework/GF.git "$REP_PRISTINE"
 fi
 
-rm -rf ${REP_CORE}
-rm -rf ${REP_RGL}
+rm -rf "$REP_CORE"
+rm -rf "$REP_RGL"
 
 # --- Begin building repos ----------------------------------------------------
 
@@ -55,15 +55,15 @@ echo
 echo "# ${REP_CORE}"
 
 echo "Copying..."
-cp -R $REP_PRISTINE $REP_CORE
+cp -R -- "$REP_PRISTINE" "$REP_CORE"
 
 echo "Cleaning..."
-cd $REP_CORE
+cd "$REP_CORE"
 git rm -r --quiet css demos doc download eclipse examples framenet gf-book treebanks *.html
 git commit -m "${COMMIT_PREFIX}Remove everything non-core" --quiet
 
-git remote set-url origin git@github.com:GrammaticalFramework/${REP_CORE}.git
-if [ $PUSH = true ]; then
+git remote set-url origin "git@github.com:GrammaticalFramework/${REP_CORE}.git"
+if [ "$PUSH" = true ]; then
   echo "Pushing..."
   git push -u origin master
 fi
@@ -77,18 +77,18 @@ echo
 echo "# ${REP_RGL}"
 
 echo "Copying..."
-cp -R $REP_PRISTINE $REP_RGL
+cp -R -- "$REP_PRISTINE" "$REP_RGL"
 
 echo "Pruning..."
-cd $REP_RGL
+cd "$REP_RGL"
 git filter-branch --prune-empty --subdirectory-filter lib master
 
 # echo "Cleaning..."
 # git rm -r --quiet doc/browse
 # git commit -m "${COMMIT_PREFIX}Remove RGL browser (separate repo)" --quiet
 
-git remote set-url origin git@github.com:GrammaticalFramework/${REP_RGL}.git
-if [ $PUSH = true ]; then
+git remote set-url origin "git@github.com:GrammaticalFramework/${REP_RGL}.git"
+if [ "$PUSH" = true ]; then
   echo "Pushing..."
   git push -u origin master
 fi
