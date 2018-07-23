89cf5a0b4263c2fdaf7ebb45dbcd9b0e5fdfd0d1
John J. Camilleri <john@digitalgrammars.com>
2018-07-23 15:09:02 +0200

    Non-Haskell RGL build script accepts --gf and --dest flags

--- split/gf-rgl/Make.sh
+++ split/gf-rgl/Make.sh
@@ -2,14 +2,43 @@
 
 # Non-Haskell RGL build script for Unix-based machines
 
-gf="gf"
-gfc="${gf} -batch -gf-lib-path=src -s "
-src="src"
-dist="dist"
-dest="/Users/john/repositories/GF/.cabal-sandbox/share/x86_64-osx-ghc-8.2.2/gf-3.9/lib"
-
 modules_langs="All Symbol Compatibility"
 modules_api="Try Symbolic"
+
+# Defaults (may be overridden by options)
+gf="gf"
+dest=""
+
+# Check command line options
+for arg in "$@"; do
+  case $arg in
+    --gf=*)
+      gf="${arg#*=}"; shift ;;
+    --dest=*)
+      dest="${arg#*=}"; shift ;;
+    *) echo "Unknown option: ${arg}" ; exit 1 ;;
+  esac
+done
+
+# Try to determine install location
+if [ -z "$dest" ]; then
+  dest="$GF_LIB_PATH"
+fi
+if [ -z "$dest" ] && [ -f "../gf-core/GF_LIB_PATH" ]; then
+  dest=`cat ../gf-core/GF_LIB_PATH`
+fi
+if [ -z "$dest" ]; then
+  echo "Unable to determine where to install the RGL. Please do one of the following:"
+  echo " - Pass the --dest=... flag to this script"
+  echo " - Set the GF_LIB_PATH environment variable"
+  echo " - Compile GF from the gf-core repository (must be in same directory as gf-rgl)"
+  exit 1
+fi
+
+# A few more definitions before we get started
+src="src"
+dist="dist"
+gfc="${gf} -batch -gf-lib-path=${src} -s "
 
 # Make directories if not present
 mkdir -p "${dist}/prelude"
